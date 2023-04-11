17c85cda2fc78ccd51bea09ea9eab945fd96c7d8
zencqui <arsenmyutuc@hotmail.com>
2023-04-12 09:54:25 +1000

    Add project files.

---
+++ RetrySample/Program.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using RetrySample;
+
+HttpClientHandler httpClientHandler = new HttpClientHandler();
+TokenRefreshHandler tokenRefreshHandler = new TokenRefreshHandler(httpClientHandler);
+
+using (HttpClient client = new HttpClient(tokenRefreshHandler))
+{
+    client.DefaultRequestHeaders.Add("Authorization", "Bearer b29f0433ccdfec3222152fb3fce4f972");
+
+    var options = new RestClientOptions("https://example.com/api/")
+    {
+        ConfigureMessageHandler = handler => tokenRefreshHandler
+    };
+    var restClient = new RestClient(options);
+
+    HttpResponseMessage response = await client.GetAsync("/endpoint");
+    Console.WriteLine(response.StatusCode);
+}
---
+++ RetrySample/TokenRefreshHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrySample
+{
+    public class TokenRefreshHandler : DelegatingHandler
+    {
+        private int _retryCount = 0;
+        private int _maxRetryCount = 3;
+        private TimeSpan _retryInterval = TimeSpan.FromSeconds(2);
+
+        public TokenRefreshHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = null;
+
+            for (_retryCount = 0; _retryCount < _maxRetryCount; _retryCount++)
+            {
+                response = await base.SendAsync(request, cancellationToken);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    break;
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    // Renew the token and update the request header
+                    string newToken = await RenewTokenAsync();
+                    request.Headers.Remove("Authorization");
+                    request.Headers.Add("Authorization", "Bearer " + newToken);
+                }
+                else
+                {
+                    break;
+                }
+
+                await Task.Delay(_retryInterval, cancellationToken);
+            }
+
+            return response;
+        }
+
+        private async Task<string> RenewTokenAsync()
+        {
+            // Renew the token here
+            // ...
+
+            return "newToken";
+        }
+    }
+}
